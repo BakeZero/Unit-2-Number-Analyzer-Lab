8dd8e9fccc3c7842745857638f05b030a353a726
harry <harry.nguyen.2016@gmail.com>
2023-04-11 09:22:47 -0400

    .

---
+++ Unit_2_Number_Analyzer_Lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Write("Welcome to the Number Analyzer, what is your name? ");
+        string name = Console.ReadLine();
+        int numInput;
+
+        while (true)
+        {
+            Console.Write($"{name}, please enter a number between 1 and 100: ");
+            numInput = int.Parse(Console.ReadLine());
+            if (numInput < 1 || numInput > 100)
+            {
+                Console.WriteLine("Invalid input, please try again.\n");
+                continue;
+            }
+
+            if (numInput % 2 == 1 && numInput < 60)
+                Console.WriteLine($"{numInput} is odd and less than 60");
+            else if (numInput % 2 == 0 && numInput >= 2 && numInput <= 24)
+                Console.WriteLine($"{numInput} is even and less than 25");
+            else if (numInput % 2 == 0 && numInput >= 26 && numInput <= 60)
+                Console.WriteLine($"{numInput} is even and between 26 and 60 inclusive");
+            else if (numInput % 2 == 0 && numInput > 60)
+                Console.WriteLine($"{numInput} is even and greater than 60");
+            else if (numInput % 2 == 1 && numInput > 60)
+                Console.WriteLine($"{numInput} is odd and greater than 60");
+
+            Console.Write("Continue? y/n: ");
+            string prompt = Console.ReadLine();
+            Console.WriteLine();
+            if (prompt == "n")
+                break;
+        }
+        Console.WriteLine("Bye!");
+    }
+}
